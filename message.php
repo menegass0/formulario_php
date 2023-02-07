543840834b94e2ec1847adfe0b1687365ae757b4
menegass0 <victormenegasso@unisantos.br>
2023-02-07 20:12:41 -0300

    final

--- message.php
+++ message.php
@@ -4,4 +4,23 @@
     $phone = $_POST['phone'];
     $website = $_POST['website'];
     $message = $_POST['message'];
+    
+    if(!empty($email) && !empty($message)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver = "yedege8294@fsouda.com";
+            $subject = "From: $name <$email>";
+            $body = "Name $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message\n\nRegards, \n$name";
+            $sender = "From $email";
+            if(mail($receiver, $subject, $body, $sender)){
+                echo "Your message has been sent";
+            }else{
+                echo "Sorry, failed to send your message";
+            }
+        }
+        else{
+            echo "Enter a valid email address!";
+        }
+    }else{
+        echo "Email and password fields are required";
+    }
 ?>
--- script.js
+++ script.js
@@ -4,16 +4,26 @@ statusTxt = form.querySelector(".button-area span");
 form.onsubmit = (e)=>{
     e.preventDefault(); //prevents form from submitting
     statusTxt.style.display= "block";
+    statusTxt.style.color = "#0D6EFD"
     //AJAX
     let xhr = new XMLHttpRequest(); //creating new xml object
     xhr.open("POST", "message.php", true);//sending post request to message.php
     xhr.onload = ()=>{//once ajax loaded
         if(xhr.readyState == 4 && xhr.status == 200){//caso o ready state seja igual a 4 e o status de resposta for 200 significa que nao há erros
             let response = xhr.response;//guardando a resposta do ajax em uma variavel
-            console.log(response);
+            if(response.indexOf("Email and password fields are required") != -1 || response.indexOf("Enter a valid email address!") != -1 || response.indexOf("Sorry, failed to send your message") != -1){
+                statusTxt.style.color = "red";
+            }
+            else{
+                form.reset;
+                setTimeout(()=>{
+                    statusTxt.style.display = "none";
+                }, 3000);//esconde o statusTxt depois de 3 segundos que a mensagem foi enviada
+            }
+            statusTxt.innerText = response;
         }
     
     }
     let formData = new FormData(form); //creating a new FormData obj 
-    xhr.send(FormData);
+    xhr.send(formData);
 }
